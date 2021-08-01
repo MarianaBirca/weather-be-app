569cc7ba652b5233cd611bcf368a2a5932bab3f9
MarianaBirca <82412276+MarianaBirca@users.noreply.github.com>
2021-08-01 21:48:55 +0100

    CSS adjustments
    Changed button & search tab properties

--- src/index.js
+++ src/index.js
@@ -58,15 +58,11 @@ function getCurrentPosition(event) {
 let currentLocation = document.querySelector(".location-button");
 currentLocation.addEventListener("click", getCurrentPosition);
 
-
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "f2385ad9d5dc3434f38126f0279324c2";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`; 
  axios.get(apiUrl).then(displayForecast);
 }
-
-
 
 function showTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
